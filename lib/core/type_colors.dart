922bfdd21ec2fab792da4d40116ed9bfb1ca6469
Angel Rodriguez <joserodriguez@multiplica.com>
2023-11-20 21:22:12 -0600

    condicion de typo de pokemon

---
+++ lib/core/type_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Map<String, Color> typeColors = {
+  "normal": Colors.grey,
+  "fighting": Colors.red.shade300,
+  "flying": Colors.lightBlue.shade100,
+  "poison": Colors.purple,
+  "ground": Colors.brown.shade500,
+  "rock": Colors.brown.shade300,
+  "bug": Colors.lightGreen,
+  "ghost": Colors.deepPurple.shade300,
+  "steel": Colors.blueGrey.shade300,
+  "fire": Colors.orange,
+  "water": Colors.blue.shade200,
+  "grass": Colors.green,
+  "electric": Colors.yellow,
+  "psychic": Colors.pink.shade200,
+  "ice": Colors.cyan.shade100,
+  "dragon": Colors.blue,
+  "dark": Colors.grey.shade800,
+  "fairy": Colors.pinkAccent,
+  "unknown": Colors.lightGreen.shade100,
+  "shadow": Colors.grey,
+};
