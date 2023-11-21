c3da0a29224b61537622d5293fa355231104b1cd
Angel Rodriguez <joserodriguez@multiplica.com>
2023-11-20 21:21:20 -0600

    consumo de servicios API

--- lib/api/pokedex_service.dart
+++ lib/api/pokedex_service.dart
@@ -1,9 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class PokedexAPI {
-  getList(int initial) {
+  String urlpokedex = dotenv.env['URL_POKEDEX']!;
+  Dio request = Dio();
+
+  Future<List<dynamic>> getList({int offset = 0}) async {
     //hacer peticion de los siguientes 10 pokemon
+    List<dynamic> response = [];
+    String finalUrl = "${urlpokedex}pokemon?limit=10&offset=$offset";
+
+    try {
+      await request
+          .get(finalUrl)
+          .then((value) => response.add(value.data["results"]));
+    } catch (e) {
+      print(e);
+    }
+    return response;
   }
 
-  getPokemonInfo(int idPokemon) {
-    //hacer peticion de informacion pokemon
+  Future<Map<String, dynamic>> getPokemonInfo({required String url}) async {
+    //hacer peticion de información pokemon
+
+    Map<String, dynamic> response = {};
+    try {
+      await request.get(url).then((value) => response = value.data);
+    } catch (e) {
+      print(e);
+    }
+    return response;
   }
 }
