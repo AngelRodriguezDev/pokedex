5687ad8c855e78f6cfdd114278702fc1cb29a7da
Angel Rodriguez <joserodriguez@multiplica.com>
2023-11-20 21:22:51 -0600

    atomizacion de componentes

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 import 'package:pokedex/controller/pokedex_controller.dart';
+import 'package:pokedex/model/pokemon_model.dart';
+import 'package:pokedex/view/widget/pokemon_card.dart';
+import 'package:pokedex/view/widget/team_dialog.dart';
+import 'package:progress_indicators/progress_indicators.dart';
 import 'package:provider/provider.dart';
 
 class PokedexPage extends StatefulWidget {
@@ -12,39 +17,64 @@ class PokedexPage extends StatefulWidget {
 class _PokedexPageState extends State<PokedexPage> {
   @override
   Widget build(BuildContext context) {
-    int countPokedex = context.watch<PokedexController>().pokeTeamCount;
+    int countPoketeam = context.watch<PokedexController>().pokeTeamCount;
+    List<Pokemon> pokedex = context.watch<PokedexController>().pokedex;
+
+    if (pokedex.isEmpty) context.read<PokedexController>().loadPokedex();
     return Scaffold(
       appBar: AppBar(
         actions: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: Image.asset(
+          TextButton(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
                   "assets/pokeball.png",
                 ),
-              ),
-              Text(countPokedex.toString(),
-                  style: TextStyle(
-                      fontSize: 28,
-                      shadows: [
-                        BoxShadow(
-                          color: Colors.black,
-                          spreadRadius: 1,
-                          blurRadius: 1,
-                          offset: Offset(3, 3),
-                        ),
-                      ],
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white)),
-            ],
-          )
+                Text(countPoketeam.toString(),
+                    style: const TextStyle(
+                        fontSize: 28,
+                        shadows: [
+                          BoxShadow(
+                            color: Colors.black,
+                            spreadRadius: 1,
+                            blurRadius: 1,
+                            offset: Offset(3, 3),
+                          ),
+                        ],
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+              ],
+            ),
+            onPressed: () => showDialog(
+                useSafeArea: true,
+                context: context,
+                builder: (context) => const TeamDialog()),
+          ),
         ],
-        title: Text("POKEDEX"),
+        title: const Text("POKEDEX"),
         backgroundColor: Colors.redAccent.shade700,
         foregroundColor: Colors.white,
       ),
+      body: pokedex.length < 10
+          ? Center(
+              child: JumpingText(
+                "...Cargando Pokedex...",
+                style: TextStyle(color: Colors.red, fontSize: 24),
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: LazyLoadScrollView(
+                child: ListView.builder(
+                  itemBuilder: (context, index) =>
+                      PokeCard(pokemon: pokedex[index]),
+                  itemCount: pokedex.length,
+                ),
+                onEndOfPage: () =>
+                    context.read<PokedexController>().loadPokedex(),
+              ),
+            ),
     );
   }
 }
